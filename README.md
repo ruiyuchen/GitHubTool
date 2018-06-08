3f843d08c96675c8cc39d182dd197bb448ec6136
ruiyuchen <adam.chen@pentium.network>
2018-06-08 11:32:26 +0800

    update github api invoke method

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # GitHubTool
 
-#### getUpdatedRepository - 取得某個organization在date時間點後有更新的GitHub repositories
+#### getUpdatedRepository - 取得某個organization的branch在date時間點後有更新的GitHub repositories
 
 ```sh
 # 執行方式
-python getUpdatedRepository.py $organization $date $username $password
+python getUpdatedRepository.py $organization $branch $date $username $password
 The format of $date should be: yyyy-mm-dd
 ```
--- getUpdatedRepository.py
+++ getUpdatedRepository.py
@@ -1,23 +1,26 @@
 import json
+import requests
 import sys
-import subprocess
 
 organization = sys.argv[1]
-date = sys.argv[2]
-username = sys.argv[3]
-password = sys.argv[4]
-credential = '{}:{}'.format(username, password)
+branch = sys.argv[2]
+date = sys.argv[3]
+username = sys.argv[4]
+password = sys.argv[5]
+
+session = requests.session()
+session.auth = (username, password)
 
 print("\n============================================================================")
-print("Get '{}' organization repositories updated after '{}'".format(organization, date))
+print("Get '{}' organization '{}' branch repositories updated after '{}'".format(organization, branch, date))
 print("Repository List")
 print("============================================================================\n")
 
 page = 1
 while True:
-    git_url = 'https://{}@api.github.com/orgs/{}/repos?page={}'.format(credential, organization, page)
-    repository_data = subprocess.check_output(["curl", git_url, "-X", "GET", "-sS"])
-    json_repository_data = json.loads(repository_data)
+    git_url = 'https://api.github.com/orgs/{}/repos?page={}'.format(organization, page)
+    repository_data = session.get(git_url)
+    json_repository_data = json.loads(repository_data.text)
 
     if json_repository_data:
         page += 1
@@ -26,15 +29,14 @@ while True:
 
     for repository in json_repository_data:
         repository_name = repository['name']
-        branch_url = 'https://{}@api.github.com/repos/{}/{}/branches/master'.format(credential, organization, repository_name)
-        branch_data = subprocess.check_output(["curl", branch_url, "-X", "GET", "-sS"])
-        json_branch_data = json.loads(branch_data)
-        repository_update_time = json_branch_data['commit']['commit']['committer']['date']
+        branch_url = 'https://api.github.com/repos/{}/{}/branches/{}'.format(organization, repository_name, branch)
+        branch_data = session.get(branch_url)
+        json_branch_data = json.loads(branch_data.text)
 
         try:
-            has_update = json_branch_data['commit']['commit']['committer']['date'] > date
-            if has_update:
-                print("{}: {}".format(repository_name, json_branch_data['commit']['commit']['committer']['date']))
+            repository_update_time = json_branch_data['commit']['commit']['committer']['date']
+            if repository_update_time > date:
+                print("{}: {}".format(repository_name, repository_update_time))
         except:
             pass
 
